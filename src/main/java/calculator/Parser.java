6f3a9ccbd0000ebf53eced6c6b8208fea8e7a344
choijw1004 <97choijw@gmail.com>
2025-10-18 23:51:13 +0900

    fix: Parser에서 \n(이스케이프된 개행) 처리 추가

--- src/main/java/calculator/Parser.java
+++ src/main/java/calculator/Parser.java
@@ -8,12 +8,27 @@ public class Parser {
     public String getDelimiter(String input){
         int start = "//".length();
         int end = input.indexOf("\n");
+
+        if(end == -1){
+            end = input.indexOf("\\n");
+        }
+
         return input.substring(start, end);
     }
 
     public String getNumbers(String input){
         int start = input.indexOf("\n");
-        return input.substring(start + 1);
+        if(start != -1){
+            return input.substring(start + 1);
+        }
+
+        start = input.indexOf("\\n");
+
+        if(start != -1){
+            return input.substring(start + 2);
+        }
+
+        throw new IllegalArgumentException();
     }
 
 }
