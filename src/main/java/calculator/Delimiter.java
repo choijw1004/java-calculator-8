a5d8b48f22861dd44c64fcd8462f85ca22287c46
choijw1004 <97choijw@gmail.com>
2025-10-18 21:21:12 +0900

    feat: Delimiter 기본 구조 생성

---
+++ src/main/java/calculator/Delimiter.java
@@ -0,0 +1,19 @@
+package calculator;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Delimiter {
+    private Set<String> delimiters;
+
+    public Delimiter(){
+    }
+
+    public void add(String customDelimiter){
+
+    }
+
+    public String[] split(String input){
+
+    }
+}
