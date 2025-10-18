5fddbd7c40e28fed3d099dfae400512c59919e2a
choijw1004 <97choijw@gmail.com>
2025-10-18 22:26:54 +0900

    fix: 입력 형식 오타 수정

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -6,7 +6,7 @@ public class Application {
     public static void main(String[] args) {
         try {
 
-            System.out.println("덧셈할 문자열을 입력해주세요");
+            System.out.println("덧셈할 문자열을 입력해 주세요.");
             String input = Console.readLine();
 
             Calculator calculator = new Calculator();
