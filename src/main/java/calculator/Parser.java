0247851fe018498ae86733ef9b2268e219730f83
choijw1004 <97choijw@gmail.com>
2025-10-18 21:38:36 +0900

    feat: Parser 기본 구조 생성

---
+++ src/main/java/calculator/Parser.java
@@ -0,0 +1,16 @@
+package calculator;
+
+public class Parser {
+    public boolean hasCustomDelimiter(String input){
+
+    }
+
+    public String getDelimiter(String input){
+
+    }
+
+    public String getNumbers(String input){
+
+    }
+
+}
