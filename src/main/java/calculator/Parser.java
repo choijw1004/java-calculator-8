aff148dae14c7e4f76e8f3be8c3097baa8ec3440
choijw1004 <97choijw@gmail.com>
2025-10-18 21:42:12 +0900

    feat: 커스텀 구분자 추출 기능 구현

--- src/main/java/calculator/Parser.java
+++ src/main/java/calculator/Parser.java
@@ -2,11 +2,13 @@ package calculator;
 
 public class Parser {
     public boolean hasCustomDelimiter(String input){
-
+        return input.startsWith("//");
     }
 
     public String getDelimiter(String input){
-
+        int start = "//".length();
+        int end = input.indexOf("\n");
+        return input.substring(start, end);
     }
 
     public String getNumbers(String input){
