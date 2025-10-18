017955a3cabb7d48381c2850087ea3f838f274f0
choijw1004 <97choijw@gmail.com>
2025-10-18 22:03:41 +0900

    feat: 예외 발생시 Consol.close()로 애플리케이션 종료하는 기능 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -4,13 +4,17 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     public static void main(String[] args) {
+        try {
 
-        System.out.println("덧셈할 문자열을 입력해주세요");
-        String input = Console.readLine();
+            System.out.println("덧셈할 문자열을 입력해주세요");
+            String input = Console.readLine();
 
-        Calculator calculator = new Calculator();
-        int result = calculator.calculate(input);
+            Calculator calculator = new Calculator();
+            int result = calculator.calculate(input);
 
-        System.out.println("결과 : " + result);
+            System.out.println("결과 : " + result);
+        } finally {
+            Console.close();
+        }
     }
 }
