37758f49130e548294cfdfa85954db8eb2fa84c7
choijw1004 <97choijw@gmail.com>
2025-10-20 12:18:44 +0900

    fix: 예외 메세지 추가

--- src/main/java/calculator/Parser.java
+++ src/main/java/calculator/Parser.java
@@ -14,13 +14,13 @@ public class Parser {
         }
 
         if(end == -1 || end <= start){
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("잘못된 커스텀 구분자 형식입니다.");
         }
 
         String delimiter = input.substring(start, end);
 
         if(delimiter.length() != 1){
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("구분자는 단일 문자여야 합니다.");
         }
 
         return delimiter;
@@ -38,7 +38,7 @@ public class Parser {
             return input.substring(start + 2);
         }
 
-        throw new IllegalArgumentException();
+        throw new IllegalArgumentException("숫자 부분을 찾을 수 없습니다.");
     }
 
 }
